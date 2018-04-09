a06671ba8237a7fb3aebcfc9163a7af771f6cf28
cnrstone123 <32866342+cnrstone123@users.noreply.github.com>
2018-04-09 14:23:02 +0900

    Add files via upload

---
+++ docs_adt/TutrActivity/MainActivityTest.java
@@ -0,0 +1,30 @@
+import android.webkit.JavascriptInterface;
+
+public class MainActivityTest extends Activity {
+
+    final Context myApp = this;
+
+    @JavascriptInterface
+    public void processHTML(String html) {
+        if (html == null)
+            return;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WebView browser = (WebView) findViewById(R.id.webview);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(this, "HTMLOUT");
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.loadUrl("javascript:window.HTMLOUT.processHTML('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+            }
+        });
+        browser.loadUrl("http://www.google.co.il");
+    }
+
+}
